cb2ebdbbc7df20ea4326f165b0a380af353d3bce
Zahid Mujaddid <archazid@outlook.com>
2023-02-16 13:44:32 +0700

    Add views and controllers of MVC and basic templates

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module archazid.io/lenslocked
 
 go 1.19
+
+require github.com/go-chi/chi/v5 v5.0.8
--- main.go
+++ main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"archazid.io/lenslocked/controllers"
+	"archazid.io/lenslocked/templates"
+	"archazid.io/lenslocked/views"
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+
+	r.Get("/", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "base.tmpl", "home.tmpl"))))
+	r.Get("/contact/", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "base.tmpl", "contact.tmpl"))))
+	r.Get("/faq/", controllers.FAQ(views.Must(
+		views.ParseFS(templates.FS, "base.tmpl", "faq.tmpl"))))
+
+	var usersC controllers.Users
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, "base.tmpl", "signup.tmpl"))
+	r.Get("/users/new/", usersC.New)
+	r.Post("/users/create/", usersC.Create)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
